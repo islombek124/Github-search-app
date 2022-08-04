03e4f018aeed5bf7074f8243f50302847e7c259e
Islombek <8845414@mail.ru>
2022-08-04 12:49:31 +0500

    Initial socials

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ icon.onclick = function(){
         icon.innerHTML = 'DARK <ion-icon name="moon"></ion-icon>';
     }
 };
+const searchBtn = document.querySelector('.search-panel button'),
+    searchInput = document.querySelector('.search-panel input');
+function apiSearch() {
+    fetch(`https://api.github.com/users/${searchInput.value}`)
+        .then(res => res.json())
+        .then(JSON => console.log(JSON));
+    searchInput.value = '';
+}
+searchBtn.addEventListener('click', apiSearch);
+window.addEventListener('keydown',(e) => {
+    if (e.code === 'Enter') {
+        apiSearch();
+    } 
+});
